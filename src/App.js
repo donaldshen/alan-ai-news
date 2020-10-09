7e6d12be5b06c16954690bd816e0df9f8d26c9e6
沈扬东 <shenyangdong@bytedance.com>
2020-10-09 20:12:47 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
 
-function App() {
+const alanKey =
+  '5d17c14f3e006785659345c9ea4ac0592e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('fuck')
+        }
+      },
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alan AI News</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
