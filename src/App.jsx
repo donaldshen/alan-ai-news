fd8eeb33ea5d026c7465c830020331994f562cc0
donald <825870831@qq.com>
2020-10-11 22:09:22 +0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,121 @@
 import React, { useEffect, useState } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 import NewsCards from './components/NewsCards.jsx'
+import { makeStyles } from '@material-ui/core/styles'
+import wordsToNumbers from 'words-to-numbers'
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    textAlign: 'center',
+    position: 'fixed',
+    left: 0,
+    bottom: 0,
+    color: 'black',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '120px',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'rgba(21, 101, 192)',
+  },
+  image: {
+    marginLeft: 20,
+  },
+  card: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    padding: '3%',
+    borderRadius: 10,
+    color: 'white',
+    backgroundColor: 'rgba(21, 101, 192)',
+    margin: '0 12px',
+    textAlign: 'center',
+    height: '25vmin',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse',
+      textAlign: 'center',
+      width: '100%',
+      height: 'initial',
+      '&:nth-of-type(1)': {
+        marginBottom: '12px',
+      },
+    },
+  },
+  infoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+  logoContainer: {
+    padding: '0 5%',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse',
+      textAlign: 'center',
+    },
+  },
+  alanLogo: {
+    height: '27vmin',
+    borderRadius: '15%',
+    padding: '0 5%',
+    margin: '3% 0',
+    [theme.breakpoints.down('sm')]: {
+      height: '35vmin',
+    },
+  },
+}))
 
 const alanKey =
   '5d17c14f3e006785659345c9ea4ac0592e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
-  const [newsArticles, setNewsArticles] = useState([{title: 'fuck'}])
+  const [newsArticles, setNewsArticles] = useState([])
+  const classes = useStyles()
+  const [activeArticle, setActiveArticle] = useState(-1)
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, n }) => {
         switch (command) {
           case 'testCommand':
             alert('testCommand')
             break
           case 'newHeadlines':
             setNewsArticles(articles)
+            setActiveArticle(-1)
             break
+          case 'highlight':
+            setActiveArticle((prevArticle) => prevArticle + 1)
+            break
+          case 'open': {
+            const parsedNumber = n.length > 2 ? wordsToNumbers(n, {fuzzy: true}) : n
+            console.log(n, parsedNumber)
+            if (parsedNumber > 20) {
+              alanBtn().playText('Please try that again')
+              return
+            }
+            setActiveArticle(parsedNumber - 1)
+            window.open(articles[parsedNumber - 1]?.url, '_blank')
+            alanBtn().playText('Opening...')
+            break
+          }
           default:
-            alert('fuck')
+            // alert('fuck')
             break
         }
       },
@@ -29,13 +124,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan AI News</h1>
-      <ul>
-        <li>What does this app do?</li>
-        <li>What can I do here?</li>
-        <li>Give me the news from $(BBC news|techcrunch)</li>
-      </ul>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt=''
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   )
 }
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,5 +1,116 @@
-import React from 'react'
+import React, { createRef, useEffect, useState } from 'react'
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@material-ui/core'
+import classNames from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default function NewsCard({title}) {
-  return <div>{title}</div>
+const useStyles = makeStyles({
+  media: {
+    height: 250,
+  },
+  border: {
+    border: 'solid',
+  },
+  fullHeightCard: {
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    borderBottom: '10px solid white',
+  },
+  activeCard: {
+    borderBottom: '10px solid #22289a',
+  },
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+})
+
+export default function NewsCard({
+  description,
+  publishedAt,
+  source,
+  title,
+  url,
+  urlToImage,
+  activeArticle,
+  i,
+}) {
+  const classes = useStyles()
+  // TODO: 一个 ref 不就行了？
+  const [elRefs, setElRefs] = useState([])
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50)
+
+  useEffect(() => {
+    setElRefs((refs) =>
+      Array(20)
+        .fill()
+        .map((_, j) => refs[j] || createRef()),
+    )
+  }, [])
+
+  useEffect(() => {
+    if (i === activeArticle && elRefs[i]) {
+      scrollToRef(elRefs[i])
+    }
+  }, [i, activeArticle, elRefs])
+
+  return (
+    <Card
+      ref={elRefs[i]}
+      className={classNames(classes.card, {
+        [classes.activeCard]: activeArticle === i,
+      })}
+    >
+      <CardActionArea href={url} target="_blank">
+        <CardMedia className={classes.media} image={urlToImage} />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  )
 }
